cab8f994e52e78f5b7717f6428f706359a19ac6a
Regi Adi Priyanto <regiadi.priyanto@gmail.com>
2023-09-12 13:28:02 +0700

    Change ID to Id and APIToken to ApiToken in models/user.go file

--- models/user.go
+++ models/user.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Id              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Usercode        string             `json:"usercode,omitempty" bson:"usercode,omitempty"`
 	Username        string             `json:"username,omitempty" bson:"username,omitempty"`
 	Password        string             `json:"password,omitempty" bson:"password,omitempty"`
@@ -11,7 +11,7 @@ type User struct {
 	Fullname        string             `json:"fullname,omitempty" bson:"fullname,omitempty"`
 	Phone           string             `json:"phone,omitempty" bson:"phone,omitempty"`
 	Address         string             `json:"address,omitempty" bson:"address,omitempty"`
-	APIToken        string             `json:"api_token,omitempty" bson:"api_token,omitempty"`
+	ApiToken        string             `json:"api_token,omitempty" bson:"api_token,omitempty"`
 	DeviceName      string             `json:"device_name,omitempty" bson:"device_name,omitempty"`
 	TokenLastUsedAt primitive.DateTime `json:"token_last_used_at,omitempty" bson:"token_last_used_at,omitempty"`
 	TokenExpiresAt  primitive.DateTime `json:"token_expires_at,omitempty" bson:"token_expires_at,omitempty"`
