d62e05487fc0e81ea4fc83fcf4cc2287ea6b0d07
Regi Adi <regiadi.priyanto@gmail.com>
2023-10-09 21:00:23 +0700

    Fix deprecated MongoDB Driver API

--- kernel/database.go
+++ kernel/database.go
@@ -2,13 +2,12 @@ package kernel
 
 import (
 	"context"
-	"log"
-	"time"
+	"fmt"
 
 	"github.com/RegiAdi/hatchet/config"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type MongoConnection struct {
@@ -19,25 +18,24 @@ type MongoConnection struct {
 var Mongo MongoConnection
 
 func NewMongoConnection() {
-	client, err := mongo.NewClient(options.Client().ApplyURI(config.GetMongoURI()))
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(config.GetMongoURI()).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// Send a ping to confirm a successful connection
+	var result bson.M
 	db := client.Database(config.GetMongoDatabase())
+
+	if err := db.RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
 	Mongo = MongoConnection{
 		Client: client,
